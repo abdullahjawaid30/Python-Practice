b63dfb7eb9e13f899a6a92dfb07d21f73d60699b
Abdullah Jawaid <abdullahjawaid30@gmail.com>
2024-05-07 02:16:21 +0500

    First Commit

---
+++ Practice.py
@@ -0,0 +1,26 @@
+# First Python Program
+print("Hello World")
+print(5)
+print(17*3)
+
+# Comments, Escape Sequences & Print Statement
+"""
+Hey Abdullah,This  
+is comment 
+Line pls donot uncomment
+"""
+
+# Hey Abdullah,This  
+# is comment 
+# Line pls donot uncomment
+
+print("Hello Abdulla ,How is your day? \nand best wish for tomorrow...")
+
+print("Hello Abdulla ,\"How is your day?\" \nand best wish for tomorrow...")
+
+print("Hey",6,7)
+print("Hey",6,7, sep="~")
+print("Hey",6,7, end="009\n")
+print("Abdullah")
+
+
