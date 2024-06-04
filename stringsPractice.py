240eda217da8769913760dbf784b099556ff4d37
Abdullah Jawaid <abdullahjawaid30@gmail.com>
2024-06-05 01:30:04 +0500

    f&Doc-Strings

--- stringsPractice.py
+++ stringsPractice.py
@@ -100,4 +100,25 @@ str1 = "His name is Dan. Dan is an honest man."
 print(str1.title())
 
 
+# f-strings in Python 
 
+letter = "Hey my name is {1} and I am from {0}"
+country = "Ethopia"
+name = "Jonathan"
+print(letter.format(country, name))
+
+print(f"Hey my name is {name} and I am from {country}")
+print(f"We use f-strings like this: Hey my name is {{name}} and I am from {{country}}")
+
+price = 49.09999
+txt = f"For only {price:.2f} dollars!"
+print(txt)
+# print(txt.format())
+print(type(f"{2 * 30}"))
+
+# Docstrings in Python
+def square(n):
+  '''Takes in a number n, returns the square of n'''
+  print(n**2)
+square(5)
+print(square.__doc__)
